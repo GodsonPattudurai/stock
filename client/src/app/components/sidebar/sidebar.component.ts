a2a134fd3c1927fa71573aa1448d593c7c8c9fa5
godson <godsonherpatt@gmail.com>
2018-06-17 18:52:46 +0530

    Include Menu item

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -9,13 +9,11 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: 'dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: 'user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: 'table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: 'typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: 'upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    { path: 'contacts', title: 'Contacts',  icon: 'contacts', class: '' },
+    { path: 'items', title: 'Items',  icon: 'layers', class: '' },
+    { path: 'sales-orders', title: 'Sales Orders',  icon: 'shopping_cart', class: '' },
+    { path: 'invoices', title: 'Invoices',  icon: 'note', class: '' },
+    { path: 'reports', title: 'Reports',  icon: 'trending_up', class: '' }
 ];
 
 @Component({
