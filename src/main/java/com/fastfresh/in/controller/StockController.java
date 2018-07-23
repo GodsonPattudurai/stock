187a641467750432324ce2aa12adf6ffd02525b8
godson <godsonherpatt@gmail.com>
2018-07-23 23:32:45 +0530

    include swagger config

--- src/main/java/com/fastfresh/in/controller/StockController.java
+++ src/main/java/com/fastfresh/in/controller/StockController.java
@@ -4,18 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fastfresh.in.model.User;
 import com.fastfresh.in.service.UserService;
 
+import io.swagger.annotations.Api;
+
 @RestController
+@Api(value="stock", description="Operations pertaining to stock")
 public class StockController {
 
 	@Autowired
 	UserService userService;
 	
-	@RequestMapping("/users")  
+	@RequestMapping(value = "/users", method = RequestMethod.GET)  
     public List<User> userList(){  
         return userService.findAll();  
     } 
